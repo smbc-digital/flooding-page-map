ff6cdd32a9f6748168a3403ccc0087433fa4b43a
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-05-04 09:48:50 +0100

    Popop:Flood Incidents Header change

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -57,7 +57,7 @@ const floodincidentsPopup = (feature, layer) => {
   const content = `<div class="smbc-map__item">
   <div class="smbc-map__item__header__block">
     <i class="fa fa-list smbc-map__item__header__block__icon" aria-hidden="true"></i>
-    <span class="smbc-map__item__header__block__title">Article 4-2 Direction</span>
+    <span class="smbc-map__item__header__block__title">Flood Incidents</span>
   </div>
   <div class="smbc-map__item__body">
     <p>Incident Date: ${feature.properties.incident_date}</p>
